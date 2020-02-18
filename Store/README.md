26be75911a9580927bfd327581ef2a39e022f4a1
vladsuxunun <47049434+vladsuxunun@users.noreply.github.com>
2020-02-18 21:33:27 +0300

    ddd

---
+++ Store/README.md
@@ -0,0 +1,3 @@
+# angular-fu1y4t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fu1y4t)
---
+++ Store/src/app/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+    window.location.href = "https://telegram.me/share/url?url=https://www.aliexpress.com/category/200216551/gaming-laptops.html&text=Laptop";
+
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
